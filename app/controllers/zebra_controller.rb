64c9b70197b1b310d54d776aa663790a87f9a249
dk4042 <dkhamaza@uchicago.edu>
2024-10-28 23:36:41 +0000

    44

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -18,4 +18,20 @@ class ZebraController < ApplicationController
     @the_result = @the_num ** (0.5)
     render({:template => "calc_template/square_root_results"})
   end
+
+  def payment
+    render({:template => "calc_template/payment"})
+  end
+
+  def payment_results
+    @t_apr = params.fetch("user_apr").to_f
+    @t_r = @t_apr / (100 * 12)
+    @t_years = params.fetch("user_years").to_i 
+    @t_n = @t_years * 12
+    @t_principal = params.fetch("user_pv").to_f
+    @denominator = 1 - (1 + @t_r) ** (-@t_n)
+    @numerator = @t_r * @t_principal
+    @the_result = @numerator / @denominator
+    render({:template => "calc_template/payment_results"})
+  end
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   get("/square/new", {:controller => "zebra", :action => "square"})
   get("/square/results", {:controller => "zebra", :action => "square_results"})
+
   get("/square_root/new", {:controller => "zebra", :action => "square_root"})
   get("/square_root/results", {:controller => "zebra", :action => "square_root_results"})
+
+  get("/payment/new", {:controller => "zebra", :action => "payment"})
+  get("/payment/results", {:controller => "zebra", :action => "payment_results"})
+
 end
