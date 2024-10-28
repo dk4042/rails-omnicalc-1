fc6c14ba64ebd7b4c34ecb70b3e8089332dc9fd5
dk4042 <dkhamaza@uchicago.edu>
2024-10-28 23:17:32 +0000

    cc

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -4,6 +4,18 @@ class ZebraController < ApplicationController
   end
 
   def square_results
+    @the_num = params.fetch("users_number").to_f
+    @the_result = @the_num ** 2
     render({:template => "calc_template/square_results"})
+  end
+
+  def square_root
+    render({:template => "calc_template/square_root"})
+  end
+
+  def square_root_results
+    @the_num = params.fetch("users_number").to_f
+    @the_result = @the_num ** (0.5)
+    render({:template => "calc_template/square_root_results"})
   end
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get("/square/new", {:controller => "zebra", :action => "square"})
   get("/square/results", {:controller => "zebra", :action => "square_results"})
+  get("/square_root/new", {:controller => "zebra", :action => "square_root"})
+  get("/square_root/results", {:controller => "zebra", :action => "square_root_results"})
 end
